a1f43d61e4a951cd8ae1933e1a4373404805f651
YannickDeBock <yannick_debock@hotmail.com>
2022-02-10 15:29:28 +0000

    Finished Task1

--- ETL.py
+++ ETL.py
@@ -1,4 +1,57 @@
+SNOWFLAKE_SOURCE_NAME = 'net.snowflake.spark.snowflake'
+from pyspark import SparkConf
 from pyspark.sql import SparkSession
+import configparser, os, boto3, pyspark.sql.functions as psf, json, net.snowflake.spark.snowflake.utils.SNOWFLAKE_SOURCE_NAME
 
-spark = SparkSession.builder.getOrCreate()
 
+config = configparser.ConfigParser()
+config.read(os.path.expanduser("~/.aws/credentials"))
+access_id = config.get('default', "aws_access_key_id") 
+access_key = config.get('default', "aws_secret_access_key")
+
+conf = SparkConf()
+conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.1.2')
+conf.set("spark.hadoop.fs.s3a.aws.credentials.provider","com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
+conf.set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+
+spark = SparkSession.builder.config(conf=conf).getOrCreate()
+
+def get_secret():
+    secret_name = "snowflake/capstone/login"
+    region_name = "eu-west-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name,
+    )
+
+    get_secret_value_response = client.get_secret_value(
+        SecretId=secret_name
+    )
+    
+    text_secret_data = get_secret_value_response['SecretString']
+    secret = json.loads(text_secret_data)
+    return secret
+
+
+secret = get_secret()
+db = secret['DATABASE']
+pw = secret['PASSWORD']
+role = secret['ROLE']
+url = secret['URL']
+user = secret['USER_NAME']
+warehouse = secret['WAREHOUSE']
+
+df = spark.read.json('s3a://dataminded-academy-capstone-resources/raw/open_aq/')
+df = df.select('*','coordinates.*','date.*').drop('coordinates','date')
+df = df.withColumn('local',psf.to_timestamp(psf.col('local')))
+df = df.withColumn('utc',psf.to_timestamp(psf.col('utc')))
+
+df.show()
+#df.write
+ #   .format(SNOWFLAKE_SOURCE_NAME)
+  #  .options(sfOptions)
+   # .option("dbtable", "air_quality")
+    #.mode('overwrite')
+    #.save()
